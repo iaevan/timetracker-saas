702b31ee699d591430d749aa12a7f636a33d649e
Ishtiak Ahmed Evan <iaevan008@gmail.com>
2026-07-21 01:00:39 +0600

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
         />
+        <meta name="apple-mobile-web-app-title" content="DailyLIne" />
       </head>
       <body>
         <a className="skip-link" href="#main">
